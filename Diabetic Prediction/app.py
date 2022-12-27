5210410eaa31092777dc7371ec720200ba9c4e35
Himanshu Kaskar <69004673+Himanshu310720@users.noreply.github.com>
2022-12-27 20:34:46 +0530

    Add files via upload

---
+++ Diabetic Prediction/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('GB_random_HT_1.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('index3.html')
+
+
+@app.route('/predict', methods=['POST','GET'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    arr = np.array([[data1, data2 ,data3, data4 , data5, data6, data7 ,data8]])
+    pred = model.predict(arr)
+    if pred == 0 :
+        return render_template('home3.html',pred="You Don't have Diabetes .")
+    else :
+        return render_template('home3.html',pred="You have Diabetes .")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
